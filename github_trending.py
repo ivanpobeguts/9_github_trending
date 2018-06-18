3553ad6fc669c7aba2a0a71cc0343a20e55f8111
Pobeguts Ivan <Ivan.Pobeguts@aton.ru>
2018-06-18 11:39:44 +0300

    requirements: update version add download repos info from github, add search_period generation add getting issues info

--- github_trending.py
+++ github_trending.py
@@ -1,11 +1,33 @@
+import requests
+from datetime import timedelta, date
 
 
-def get_trending_repositories(top_size):
-    pass
+def get_trending_repositories(search_period, top_size=20):
+    base_url = 'https://api.github.com/search/repositories'
+    params = {
+        'q': 'created:{}'.format(search_period),
+        'sort': 'stars',
+        'order': 'desc',
+    }
+    r = requests.get(base_url, params=params)
+    return r.json()['items'][:top_size]
 
 
-def get_open_issues_amount(repo_owner, repo_name):
-    pass
+def get_open_issues_amount(repo_full_name):
+    base_url = 'https://api.github.com/repos/{}/issues'.format(repo_full_name)
+    r = requests.get(base_url)
+    return len(r.json())
+
+
+def get_search_period(delta=7):
+    return date.today() - timedelta(delta)
+
 
 if __name__ == '__main__':
-    pass
+    search_period = get_search_period()
+    try:
+        top_repos = get_trending_repositories(search_period)
+        for repo in top_repos:
+            print(repo['full_name'], ': ', repo['stargazers_count'], sep='')
+    except KeyError:
+        print('Query limit exceeded. Try in a minute')
